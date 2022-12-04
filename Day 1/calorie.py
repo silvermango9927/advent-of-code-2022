9cf752e68e6c223e5d11420c5bcac8281cfcb8a5
Abhay Ganti <abhayganti0909@gmail.com>
2022-12-04 10:33:09 +0800

    optimized

--- Day 1/calorie.py
+++ Day 1/calorie.py
@@ -1,2 +1,5 @@
+# Reading the file, and calculating the total calories
 cal=[sum(map(int, p.split('\n'))) for p in open('Day 1/input.txt', 'r').read().strip().split('\n\n')]
+# Printing position of the elf and the total number of calories
 print(cal.index(sorted(cal)[-1]), sorted(cal)[-1])
+### WON MY FIRST GOLD STAR WOOHOO !
